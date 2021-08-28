05ea8fac65c1395510a0752107d4288ae51a6e79
Abul Qasim <abulqasim110@gmail.com>
2021-08-28 11:37:57 -0400

    start

---
+++ firstrb.rb
@@ -0,0 +1,21 @@
+
+# # Comments go here
+# # create var
+# firstMessage = "Puts Hello on console when you call 'ruby firstrb.rb'"
+# firstNum = 34
+# puts firstMessage +" " + firstNum.to_s
+
+# # rubby knows its inside Quotes so no need to convert
+# puts "#{firstMessage} text like CF My nume is #{firstNum}"
+
+# # firstInput = gets
+# # puts "--User Input #{firstInput.capitalize}------"
+# # ! after method calls means permanent change. 
+# puts firstMessage.upcase!
+
+# puts firstMessage.downcase
+
+# puts firstMessage
+
+# # ! get methods
+# puts String.methods
